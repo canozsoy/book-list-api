1d06f107f3a2a6686c715c523ecd9f833d33db47
canozsoy <cnozsy@gmail.com>
2021-11-20 22:31:13 +0300

    default error handler is corrected

--- controllers/error_handler.js
+++ controllers/error_handler.js
@@ -1,5 +1,8 @@
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
     console.error(err);
+    if (res.headersSent) {
+        return next(err);
+    }
     return res.status(500).json({
         error: err,
     });
