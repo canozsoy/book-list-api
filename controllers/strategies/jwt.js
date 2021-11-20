b349608f185062739f177042da777c7b64ea8c22
canozsoy <cnozsy@gmail.com>
2021-11-20 22:31:48 +0300

    jwt validation is implemented

--- controllers/strategies/jwt.js
+++ controllers/strategies/jwt.js
@@ -1,11 +1,27 @@
 const jwt = require('jsonwebtoken');
 
+function extractJWTFromBearerToken(req) {
+    const bearerToken = req.headers.authorization;
+    if (!bearerToken || !bearerToken.includes('Bearer ')) {
+        return null;
+    }
+    return bearerToken.replace('Bearer ', '');
+}
+
 const createJWT = (username) => jwt.sign({ username }, process.env.JWT_SECRET, {
     expiresIn: 86400,
 });
 
-const validateJWT = () => {
+const validateJWT = async (req, res, next) => {
+    const token = extractJWTFromBearerToken(req);
 
+    try {
+        await jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.sendStatus(401);
+    }
+
+    return next();
 };
 
 module.exports = {
--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { validateJWT } = require('../controllers/strategies/jwt');
 
 const router = express.Router();
+router.use(validateJWT);
 
 router.get('/', (req, res) => {
     res.sendStatus(200);
